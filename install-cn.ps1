a0f1c9dde13b33f72ce1f1cc59a726a29cf24b07
akiba <akibanzu@outlook.com>
2023-01-23 11:07:50 +0800

    fix install

--- install-cn.ps1
+++ install-cn.ps1
@@ -1,15 +1,17 @@
 $Env:HF_HOME="huggingface"
 
-Write-Output "姝ｅ湪鍒涘缓铏氭嫙鐜..."
-python -m venv venv
+if (!(Test-Path -Path "venv")) {
+    Write-Output "正在创建虚拟环境..."
+    python -m venv venv
+}
 .\venv\Scripts\activate
 
-Write-Output "瀹夎绋嬪簭鎵�闇�渚濊禆..."
-pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 --extra-index-url https://mirror.sjtu.edu.cn/pytorch-wheels/torch_stable.html
-pip install --upgrade -r requirements.txt -i https://mirrors.bfsu.edu.cn/pypi/web/simple
+Write-Output "安装程序所需依赖 (已进行国内加速，若无法使用加速源请用)..."
+pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 --extra-index-url https://mirror.sjtu.edu.cn/pytorch-wheels
+pip install --upgrade -r ./sd-scripts/requirements.txt -i https://mirrors.bfsu.edu.cn/pypi/web/simple
 pip install -U -I --no-deps https://jihulab.com/api/v4/projects/82097/packages/pypi/files/4108c84a1bd08244048b6d71005be25aba081839da4fbc801682c5df2bec1c9e/xformers-0.0.14.dev0-cp310-cp310-win_amd64.whl
 
-Write-Output "瀹夎 bitsandbytes..."
+Write-Output "安装 bitsandbytes..."
 cp .\sd-scripts\bitsandbytes_windows\*.dll .\venv\Lib\site-packages\bitsandbytes\
 cp .\sd-scripts\bitsandbytes_windows\cextension.py .\venv\Lib\site-packages\bitsandbytes\cextension.py
 cp .\sd-scripts\bitsandbytes_windows\main.py .\venv\Lib\site-packages\bitsandbytes\cuda_setup\main.py
--- install.ps1
+++ install.ps1
@@ -6,7 +6,7 @@ python -m venv venv
 
 Write-Output "Installing deps..."
 pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 --extra-index-url https://download.pytorch.org/whl/cu116
-pip install --upgrade -r requirements.txt
+pip install --upgrade -r ./sd-scripts/requirements.txt
 pip install -U -I --no-deps https://github.com/C43H66N12O12S2/stable-diffusion-webui/releases/download/f/xformers-0.0.14.dev0-cp310-cp310-win_amd64.whl
 
 Write-Output "Installing bitsandbytes for windows..."
