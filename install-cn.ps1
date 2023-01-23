28ab30bd5d5dc761102df1d8ae392e1ebe85fcd6
akiba <akibanzu@outlook.com>
2023-01-23 13:45:34 +0800

    fix install-cn xformers

--- install-cn.ps1
+++ install-cn.ps1
@@ -6,11 +6,11 @@ if (!(Test-Path -Path "venv")) {
 }
 .\venv\Scripts\activate
 
-Write-Output "安装程序所需依赖 (已进行国内加速，若无法使用加速源请用 install.ps1 )..."
+Write-Output "安装程序所需依赖 (已进行国内加速，若无法使用加速源请用 install.ps1)..."
 Set-Location .\sd-scripts
 pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 --extra-index-url https://mirror.sjtu.edu.cn/pytorch-wheels
 pip install --upgrade -r requirements.txt -i https://mirrors.bfsu.edu.cn/pypi/web/simple
-pip install -U -I --no-deps https://jihulab.com/api/v4/projects/82097/packages/pypi/files/4108c84a1bd08244048b6d71005be25aba081839da4fbc801682c5df2bec1c9e/xformers-0.0.14.dev0-cp310-cp310-win_amd64.whl
+pip install -U -I --no-deps https://jihulab.com/api/v4/projects/82097/packages/pypi/files/e8508fe14c8f2552a822f5e6f5620b24fdd4ba3129c2a31a39b56425bcc023bc/xformers-0.0.14.dev0+torch12-cp310-cp310-win_amd64.whl
 
 Write-Output "安装 bitsandbytes..."
 cp .\bitsandbytes_windows\*.dll ..\venv\Lib\site-packages\bitsandbytes\
