99a678bcda91dc8a897b646eb95c06be61b79b40
Akiba <akiba@anzu.link>
2023-01-24 16:25:01 +0800

    fix linux install

--- install.bash
+++ install.bash
@@ -5,6 +5,7 @@ echo "Installing deps..."
 cd ./sd-scripts
 pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 --extra-index-url https://download.pytorch.org/whl/cu116
 pip install --upgrade -r requirements.txt
-pip install -U -I --no-deps https://github.com/C43H66N12O12S2/stable-diffusion-webui/releases/download/f/xformers-0.0.14.dev0-cp310-cp310-win_amd64.whl
+pip install git+https://github.com/facebookresearch/xformers.git@0bad001ddd56c080524d37c84ff58d9cd030ebfd
+pip install triton==2.0.0.dev20221202
 
 echo "Install completed"
