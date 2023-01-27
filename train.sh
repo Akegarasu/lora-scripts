90469c5bb270d76cd6757d0d098846cb3c3fb56b
Akiba <akiba@anzu.link>
2023-01-27 10:26:44 +0800

    useless export

--- train.sh
+++ train.sh
@@ -1,29 +1,29 @@
 # LoRA train script by @Akegarasu
 
 # Train data path | 设置训练用模型、图片
-export pretrained_model="./sd-models/model.ckpt" # base model path | 底模路径
-export train_data_dir="./train/aki" # train dataset path | 训练数据集路径
+pretrained_model="./sd-models/model.ckpt" # base model path | 底模路径
+train_data_dir="./train/aki" # train dataset path | 训练数据集路径
 
 # Train related params | 训练相关参数
-export resolution="512,512" # image resolution w,h. 图片分辨率，宽,高。支持非正方形，但必须是 64 倍数。
-export batch_size=1 # batch size
-export max_train_epoches=10 # max train epoches | 最大训练 epoch
-export save_every_n_epochs=2 # save every n epochs | 每 N 个 epoch 保存一次
-export network_dim=32 # network dim | 常用 4~128，不是越大越好
-export network_alpha=32 # network alpha | 常用与 network_dim 相同的值或者采用较小的值，如 network_dim的一半 防止下溢。默认值为 1，使用较小的 alpha 需要提升学习率。
-export clip_skip=2 # clip skip | 玄学 一般用 2
-export train_unet_only=0 # train U-Net only | 仅训练 U-Net，开启这个会牺牲效果大幅减少显存使用。6G显存可以开启
-export train_text_encoder_only=0 # train Text Encoder only | 仅训练 文本编码器
+resolution="512,512" # image resolution w,h. 图片分辨率，宽,高。支持非正方形，但必须是 64 倍数。
+batch_size=1 # batch size
+max_train_epoches=10 # max train epoches | 最大训练 epoch
+save_every_n_epochs=2 # save every n epochs | 每 N 个 epoch 保存一次
+network_dim=32 # network dim | 常用 4~128，不是越大越好
+network_alpha=32 # network alpha | 常用与 network_dim 相同的值或者采用较小的值，如 network_dim的一半 防止下溢。默认值为 1，使用较小的 alpha 需要提升学习率。
+clip_skip=2 # clip skip | 玄学 一般用 2
+train_unet_only=0 # train U-Net only | 仅训练 U-Net，开启这个会牺牲效果大幅减少显存使用。6G显存可以开启
+train_text_encoder_only=0 # train Text Encoder only | 仅训练 文本编码器
 
 # Learning rate | 学习率
-export lr="1e-4"
-export unet_lr="1e-4"
-export text_encoder_lr="1e-5"
-export lr_scheduler="cosine_with_restarts" # "linear", "cosine", "cosine_with_restarts", "polynomial", "constant", "constant_with_warmup"
+lr="1e-4"
+unet_lr="1e-4"
+text_encoder_lr="1e-5"
+lr_scheduler="cosine_with_restarts" # "linear", "cosine", "cosine_with_restarts", "polynomial", "constant", "constant_with_warmup"
 
 # Output settings | 输出设置
-export output_name="aki" # output model name | 模型保存名称
-export save_model_as="safetensors" # model save ext | 模型保存格式 ckpt, pt, safetensors
+output_name="aki" # output model name | 模型保存名称
+save_model_as="safetensors" # model save ext | 模型保存格式 ckpt, pt, safetensors
 
 
 
