b64c3f2454fbde178eec0b181708ca2eaf95d89c
青龍聖者@bdsqlsz <qinglongshengzhe@gmail.com>
2023-03-01 20:36:08 +0800

    Update interrogate.ps1

--- interrogate.ps1
+++ interrogate.ps1
@@ -1,8 +1,8 @@
 # LoRA interrogate script by @bdsqlsz
 
 $v2 = 0 # load Stable Diffusion v2.x model / Stable Diffusion 2.x模型读取
-$sd_model = "./sd-models/yabukiKentaroStyle_yabukiFinalV10.safetensors" # Stable Diffusion model to load: ckpt or safetensors file | 读取的基础SD模型, 保存格式 cpkt 或 safetensors
-$model = "./output/tarot-000010.safetensors" # LoRA model to interrogate: ckpt or safetensors file | 需要调查关键字的LORA模型, 保存格式 cpkt 或 safetensors
+$sd_model = "./sd-models/sd_model.safetensors" # Stable Diffusion model to load: ckpt or safetensors file | 读取的基础SD模型, 保存格式 cpkt 或 safetensors
+$model = "./output/LoRA.safetensors" # LoRA model to interrogate: ckpt or safetensors file | 需要调查关键字的LORA模型, 保存格式 cpkt 或 safetensors
 $batch_size = 64 # batch size for processing with Text Encoder | 使用 Text Encoder 处理时的批量大小，默认16，推荐64/128
 $clip_skip = 1 # use output of nth layer from back of text encoder (n>=1) | 使用文本编码器倒数第 n 层的输出，n 可以是大于等于 1 的整数
 
