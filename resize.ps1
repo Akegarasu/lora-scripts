8968bfe49cd339522bc8edd23c64acce647d8bb9
青龍聖者@bdsqlsz <qinglongshengzhe@gmail.com>
2023-03-13 11:46:28 +0800

    更新动态调整方式

--- resize.ps1
+++ resize.ps1
@@ -6,6 +6,8 @@ $model = "./output/lora_name.safetensors" # original LoRA model path need to res
 $save_to = "./output/lora_name_new.safetensors" # output LoRA model path, save as ckpt or safetensors | 输出路径, 保存格式 cpkt 或 safetensors
 $device = "cuda" # device to use, cuda for GPU | 使用 GPU跑, 默认 CPU
 $verbose = 1 # display verbose resizing information | rank变更时, 显示详细信息
+$dynamic_method = "" # Specify dynamic resizing method, --new_rank is used as a hard limit for max rank | 动态调节大小，可选"sv_ratio", "sv_fro", "sv_cumulative",默认无
+$dynamic_param = "" # Specify target for dynamic reduction | 动态参数,sv_ratio模式推荐1~2, sv_cumulative模式0~1, sv_fro模式0~1, 比sv_cumulative要高
 
 
 # Activate python venv
@@ -16,6 +18,14 @@ $ext_args = [System.Collections.ArrayList]::new()
 
 if ($verbose) {
   [void]$ext_args.Add("--verbose")
+}
+
+if ($dynamic_method) {
+  [void]$ext_args.Add("--dynamic_method=" + $dynamic_method)
+}
+
+if ($dynamic_param) {
+  [void]$ext_args.Add("--dynamic_param=" + $dynamic_param)
 }
 
 # run resize
