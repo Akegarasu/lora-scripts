0d5810eb8695f3f8bf5fe35496fb41c8016cace5
LinXudong <sjzlxd@qq.com>
2023-03-25 14:50:23 +0800

    Added sample file output and other options support

--- install-cn.ps1
+++ install-cn.ps1
@@ -28,16 +28,17 @@ Check "激活虚拟环境失败。"
 
 Write-Output "安装程序所需依赖 (已进行国内加速，若无法使用加速源请用 install.ps1)..."
 Set-Location .\sd-scripts
-pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 -f https://mirror.sjtu.edu.cn/pytorch-wheels/torch_stable.html -i https://mirrors.bfsu.edu.cn/pypi/web/simple
+pip install torch==1.13.1+cu117 torchvision==0.14.1+cu117 -f https://mirror.sjtu.edu.cn/pytorch-wheels/torch_stable.html -i https://mirrors.bfsu.edu.cn/pypi/web/simple
 Check "torch 安装失败，请删除 venv 文件夹后重新运行。"
 pip install --upgrade -r requirements.txt -i https://mirrors.bfsu.edu.cn/pypi/web/simple
 Check "其他依赖安装失败。"
-pip install -U -I --no-deps https://jihulab.com/api/v4/projects/82097/packages/pypi/files/e8508fe14c8f2552a822f5e6f5620b24fdd4ba3129c2a31a39b56425bcc023bc/xformers-0.0.14.dev0+torch12-cp310-cp310-win_amd64.whl
+#使用xformers0.0.17 会有这个错误提示，但是不影响使用 Error caught was: No module named 'triton'
+pip install xformers==0.0.17.dev473 -i https://mirrors.bfsu.edu.cn/pypi/web/simple
 Check "xformers 安装失败。"
 pip install --upgrade lion-pytorch -i https://mirrors.bfsu.edu.cn/pypi/web/simple
 Check "Lion 优化器安装失败。"
 pip install --upgrade lycoris-lora -i https://mirrors.bfsu.edu.cn/pypi/web/simple
-Check "lycoris 安装失败。"
+Check "lycoris-lora 安装失败。"
 
 
 Write-Output "安装 bitsandbytes..."
