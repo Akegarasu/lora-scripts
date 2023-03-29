5ecee00d33ddd0663081fcd9977242fdc78923e2
Akiba <akiba@anzu.link>
2023-03-29 22:48:59 +0800

    update install.ps1

--- install.ps1
+++ install.ps1
@@ -8,9 +8,9 @@ if (!(Test-Path -Path "venv")) {
 
 Write-Output "Installing deps..."
 Set-Location .\sd-scripts
-pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 --extra-index-url https://download.pytorch.org/whl/cu116
+pip install torch==2.0.0+cu118 torchvision==0.15.1+cu118 --extra-index-url https://download.pytorch.org/whl/cu118
 pip install --upgrade -r requirements.txt
-pip install -U -I --no-deps https://github.com/C43H66N12O12S2/stable-diffusion-webui/releases/download/f/xformers-0.0.14.dev0-cp310-cp310-win_amd64.whl
+pip install --upgrade xformers==0.0.17
 
 Write-Output "Installing bitsandbytes for windows..."
 cp .\bitsandbytes_windows\*.dll ..\venv\Lib\site-packages\bitsandbytes\
