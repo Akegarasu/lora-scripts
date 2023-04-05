44dbd398f3294797586ae34e371e84bfd972bfe8
Akiba <akiba@anzu.link>
2023-04-05 16:59:11 +0800

    update install-cn

--- install-cn.ps1
+++ install-cn.ps1
@@ -26,14 +26,18 @@ if (!(Test-Path -Path "venv")) {
 .\venv\Scripts\activate
 Check "激活虚拟环境失败。"
 
-Write-Output "安装程序所需依赖 (已进行国内加速，若在国外或无法使用加速源请换用 install.ps1 脚本)"
 Set-Location .\sd-scripts
-pip install torch==2.0.0+cu118 torchvision==0.15.1+cu118 -f https://mirror.sjtu.edu.cn/pytorch-wheels/torch_stable.html -i https://mirrors.bfsu.edu.cn/pypi/web/simple
-Check "torch 安装失败，请删除 venv 文件夹后重新运行。"
+Write-Output "安装程序所需依赖 (已进行国内加速，若在国外或无法使用加速源请换用 install.ps1 脚本)"
+$install_torch = Read-Host "是否需要安装 Torch+xformers? 若您本次为首次安装请选择 y ，若本次为升级依赖安装则选择 n。[y/n] (默认为 y)"
+if ($install_torch -eq "y" -or $install_torch -eq "Y" -or $install_torch -eq ""){
+    pip install torch==2.0.0+cu118 torchvision==0.15.1+cu118 -f https://mirror.sjtu.edu.cn/pytorch-wheels/torch_stable.html -i https://mirrors.bfsu.edu.cn/pypi/web/simple
+    Check "torch 安装失败，请删除 venv 文件夹后重新运行。"
+    pip install -U -I --no-deps xformers==0.0.17 -i https://mirrors.aliyun.com/pypi/simple/
+    Check "xformers 安装失败。"
+}
+
 pip install --upgrade -r requirements.txt -i https://mirrors.bfsu.edu.cn/pypi/web/simple
 Check "其他依赖安装失败。"
-pip install -U -I --no-deps xformers==0.0.17rc482 -i https://mirrors.aliyun.com/pypi/simple/
-Check "xformers 安装失败。"
 pip install --upgrade lion-pytorch -i https://mirrors.bfsu.edu.cn/pypi/web/simple
 Check "Lion 优化器安装失败。"
 pip install --upgrade lycoris-lora -i https://mirrors.bfsu.edu.cn/pypi/web/simple
