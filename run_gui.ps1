b09fc793ccdeadb389f66b6cce46a0f0a42cfd3e
lkwfive <lkwfive@gmail.com>
2023-05-19 10:50:37 +0800

    Update run_gui.ps1
    Add host and port parameters

--- run_gui.ps1
+++ run_gui.ps1
@@ -1,6 +1,14 @@
+param(
+  [Parameter(Mandatory=$false)]
+  [string]$hostname = "127.0.0.1",
+
+  [Parameter(Mandatory=$false)]
+  [string]$port = "28000"
+)
+
 .\venv\Scripts\activate
 
 $Env:HF_HOME = "huggingface"
 $Env:PYTHONUTF8 = "1"
 
-python gui.py
+python gui.py $hostname $port
