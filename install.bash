72a76b0647d9a074502faf458586baf744be79bd
lizhou.zhu <rolandlivingstone@gmail.com>
2023-05-21 00:30:48 +0800

    Improve install.bash (#117)

--- install.bash
+++ install.bash
@@ -1,34 +1,54 @@
-echo "Creating python venv..."
-python3 -m venv venv
-source venv/bin/activate
+#!/usr/bin/bash
 
-echo "Installing torch & xformers..."
-printf 'Which version of torch do you want to install?
-(1) torch 2.0.0+cu118 with xformers 0.0.17 (suggested)
-(2) torch 1.12.1+cu116, with xformers 0bad001ddd56c080524d37c84ff58d9cd030ebfd
-'
-while true; do
-    read -p "Choose: " version
-    case $version in
-    [1]*)
-        pip install torch==2.0.0+cu118 torchvision==0.15.1+cu118 --extra-index-url https://download.pytorch.org/whl/cu118
-        pip install xformers==0.0.19
-        break
-        ;;
-    [2]*)
-        pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 --extra-index-url https://download.pytorch.org/whl/cu116
-        pip install --upgrade git+https://github.com/facebookresearch/xformers.git@0bad001ddd56c080524d37c84ff58d9cd030ebfd
-        pip install triton==2.0.0.dev20221202
-        break
-        ;;
-    *) echo "Please enter 1 or 2." ;;
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+create_venv=true
+
+while [ -n "$1" ]; do
+    case "$1" in
+        --disable-venv)
+            create_venv=false
+            shift
+            ;;
+        *)
+            shift
+            ;;
     esac
 done
 
+if $create_venv; then
+    echo "Creating python venv..."
+    python3 -m venv venv
+    source "$script_dir/venv/bin/activate"
+    echo "active venv"
+fi
+
+echo "Installing torch & xformers..."
+
+cuda_version_line=$(nvcc --version | grep 'release')
+cuda_version=$(echo $cuda_version_line | sed -n -e 's/^.*release \([0-9]\+\.[0-9]\+\),.*$/\1/p')
+
+echo "Cuda Version:$cuda_version"
+
+if [[ $cuda_version == "11.8" ]]; then
+    echo "install torch 2.0.0+cu118"
+    pip install torch==2.0.0+cu118 torchvision==0.15.1+cu118 --extra-index-url https://download.pytorch.org/whl/cu118
+    pip install xformers==0.0.19
+elif [[ $cuda_version == "11.6" ]]; then
+    echo "install torch 1.12.1+cu116"
+    pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 --extra-index-url https://download.pytorch.org/whl/cu116
+    pip install --upgrade git+https://github.com/facebookresearch/xformers.git@0bad001ddd56c080524d37c84ff58d9cd030ebfd
+    pip install triton==2.0.0.dev20221202
+else
+    echo "Unsupported cuda version:$cuda_version"
+    exit 1
+fi
+
 echo "Installing deps..."
-cd ./sd-scripts
+cd "$script_dir/sd-scripts" || exit
 
 pip install --upgrade -r requirements.txt
 pip install --upgrade lion-pytorch lycoris-lora dadaptation fastapi uvicorn wandb
+
+cd "$script_dir" || exit
 
 echo "Install completed"
