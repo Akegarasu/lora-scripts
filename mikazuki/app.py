192ac97f81909474dd9694527e6911f9b38cf1c4
akiba <akiba@anzu.link>
2023-08-11 16:18:11 +0800

    fix

--- mikazuki/app.py
+++ mikazuki/app.py
@@ -92,8 +92,13 @@ async def create_toml_file(request: Request, background_tasks: BackgroundTasks):
 
     utils.prepare_requirements()
     suggest_cpu_threads = 8 if utils.get_total_images(j["train_data_dir"]) > 100 else 2
-    trainer_file = "./sd-scripts/sdxl_train_network.py" if j["model_train_type"] == "sdxl-lora" else "./sd-scripts/train_network.py"
-    del j["model_train_type"]
+    trainer_file = "./sd-scripts/sdxl_train_network.py"
+
+    if "model_train_type" in j:
+        if j.get("model_train_type") == "sdxl-lora":
+            trainer_file = "./sd-scripts/train_network.py"
+        del j["model_train_type"]
+
     with open(toml_file, "w") as f:
         f.write(toml.dumps(j))
     background_tasks.add_task(run_train, toml_file, trainer_file, suggest_cpu_threads)
