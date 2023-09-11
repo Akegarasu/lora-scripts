f82500855ede77518718db20d8982d4f7c4b5faf
akiba <akiba@anzu.link>
2023-09-11 20:58:55 +0800

    update install script

--- install.bash
+++ install.bash
@@ -31,9 +31,9 @@ cuda_minor_version=$(echo "$cuda_version" | awk -F'.' '{print $2}')
 echo "Cuda Version:$cuda_version"
 
 if (( cuda_major_version >= 12 )) || (( cuda_major_version == 11 && cuda_minor_version >= 8 )); then
-    echo "install torch 2.0.0+cu118"
-    pip install torch==2.0.0+cu118 torchvision==0.15.1+cu118 --extra-index-url https://download.pytorch.org/whl/cu118
-    pip install xformers==0.0.19
+    echo "install torch 2.0.1+cu118"
+    pip install torch==2.0.1+cu118 torchvision==0.15.1+cu118 --extra-index-url https://download.pytorch.org/whl/cu118
+    pip install xformers==0.0.21
 elif (( cuda_major_version == 11 && cuda_minor_version == 6 )); then
     echo "install torch 1.12.1+cu116"
     pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 --extra-index-url https://download.pytorch.org/whl/cu116
--- install.ps1
+++ install.ps1
@@ -11,14 +11,13 @@ Set-Location .\sd-scripts
 pip install torch==2.0.0+cu118 torchvision==0.15.1+cu118 --extra-index-url https://download.pytorch.org/whl/cu118
 pip install -U -I --no-deps xformers==0.0.19
 
+Write-Output "Installing bitsandbytes for windows..."
+pip install bitsandbytes==0.41.1 --index-url https://jllllll.github.io/bitsandbytes-windows-webui
+
 pip install --upgrade -r requirements.txt
 
-Write-Output "Installing bitsandbytes for windows..."
-cp .\bitsandbytes_windows\*.dll ..\venv\Lib\site-packages\bitsandbytes\
-cp .\bitsandbytes_windows\cextension.py ..\venv\Lib\site-packages\bitsandbytes\cextension.py
-cp .\bitsandbytes_windows\main.py ..\venv\Lib\site-packages\bitsandbytes\cuda_setup\main.py
-
-pip install --upgrade lion-pytorch dadaptation prodigyopt lycoris-lora fastapi uvicorn wandb
+Set-Location ..
+pip install --upgrade -r requirements_win.txt
 
 Write-Output "Install completed"
 Read-Host | Out-Null ;
