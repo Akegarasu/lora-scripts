87c8d49673c711ba81c735b6348a256ccd7d615a
Akegarasu <akiba@anzu.link>
2024-04-15 15:38:10 +0800

    resolve routing issues

--- mikazuki/app/application.py
+++ mikazuki/app/application.py
@@ -1,23 +1,35 @@
 import asyncio
 import mimetypes
 import os
-import webbrowser
 import sys
+import webbrowser
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+from starlette.exceptions import HTTPException
 
-from mikazuki.utils.devices import check_torch_gpu
 from mikazuki.app.api import load_schemas
 from mikazuki.app.api import router as api_router
 # from mikazuki.app.ipc import router as ipc_router
 from mikazuki.app.proxy import router as proxy_router
+from mikazuki.utils.devices import check_torch_gpu
 
 mimetypes.add_type("application/javascript", ".js")
 mimetypes.add_type("text/css", ".css")
+
+
+class SPAStaticFiles(StaticFiles):
+    async def get_response(self, path: str, scope):
+        try:
+            return await super().get_response(path, scope)
+        except HTTPException as ex:
+            if ex.status_code == 404:
+                return await super().get_response("index.html", scope)
+            else:
+                raise ex
 
 
 async def app_startup():
@@ -67,4 +79,4 @@ async def index():
     return FileResponse("./frontend/dist/index.html")
 
 
-app.mount("/", StaticFiles(directory="frontend/dist"), name="static")
+app.mount("/", SPAStaticFiles(directory="frontend/dist", html=True), name="static")
