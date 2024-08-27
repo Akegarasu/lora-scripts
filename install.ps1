c301ffd2fe0ae7a53f123703f82893f712ba53d0
Akegarasu <akiba@anzu.link>
2024-08-27 15:44:56 +0800

    update install

--- install.ps1
+++ install.ps1
@@ -8,7 +8,7 @@ if (!(Test-Path -Path "venv")) {
 
 Write-Output "Installing deps..."
 Set-Location .\sd-scripts
-pip install torch==2.4.0+cu121 torchvision==0.19.0+cu121 --extra-index-url https://download.pytorch.org/whl/cu121
+pip install torch==2.4.0 torchvision==0.19.0 --extra-index-url https://download.pytorch.org/whl/cu121
 pip install -U -I --no-deps xformers==0.0.27.post2
 pip install --upgrade -r requirements.txt
 
