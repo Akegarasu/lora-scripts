58c3d4d0d18b99e94153406816e150cdd10474d0
PAseer <128199180+NBSTpeterhill@users.noreply.github.com>
2024-08-29 21:00:38 +0200

    Update run_gui.sh     FROM PAseer  Solve T5XXL不能加载问题
    替换Huggingface镜像源，解决T5XXL不能加载的问题
    希望能够作为可选项。

--- run_gui.sh
+++ run_gui.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-export HF_HOME=huggingface
+#export HF_HOME=huggingface
+export HF_ENDPOINT=https://hf-mirror.com  #为国内网络环境做替换，解决无法识别t5xxl-V1的问题
 export PYTHONUTF8=1
 
 python gui.py "$@"
