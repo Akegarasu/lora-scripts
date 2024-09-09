f7af1642c6a11a70b0c3b79da0a3a100c6f80618
Akegarasu <akiba@anzu.link>
2024-09-09 23:03:01 +0800

    script

--- install-cn.ps1
+++ install-cn.ps1
@@ -17,31 +17,38 @@ function Check {
         InstallFail
     }
 }
-
-if (!(Test-Path -Path "venv")) {
-    Write-Output "正在创建虚拟环境..."
-    python -m venv venv
-    Check "创建虚拟环境失败，请检查 python 是否安装完毕以及 python 版本是否为64位版本的python 3.10、或python的目录是否在环境变量PATH内。"
+if (Test-Path -Path "python\python.exe") {
+    Write-Output "使用 python 文件夹内的 python..."
+    $py_path = (Get-Item "python").FullName
+    $env:PATH = "$py_path;$env:PATH"
 }
-
-.\venv\Scripts\activate
-Check "激活虚拟环境失败。"
+else {
+    if (!(Test-Path -Path "venv")) {
+        Write-Output "正在创建虚拟环境..."
+        python -m venv venv
+        Check "创建虚拟环境失败，请检查 python 是否安装完毕以及 python 版本是否为64位版本的python 3.10、或python的目录是否在环境变量PATH内。"
+    }
+    
+    Write-Output "检测到虚拟环境，尝试激活..."
+    .\venv\Scripts\activate
+    Check "激活虚拟环境失败。"
+}
 
 Set-Location .\scripts
 Write-Output "安装程序所需依赖 (已进行国内加速，若在国外或无法使用加速源请换用 install.ps1 脚本)"
 $install_torch = Read-Host "是否需要安装 Torch+xformers? [y/n] (默认为 y)"
-if ($install_torch -eq "y" -or $install_torch -eq "Y" -or $install_torch -eq ""){
-    pip install torch==2.4.0+cu121 torchvision==0.19.0+cu121 -f https://mirror.sjtu.edu.cn/pytorch-wheels/torch_stable.html
+if ($install_torch -eq "y" -or $install_torch -eq "Y" -or $install_torch -eq "") {
+    python -m pip install torch==2.4.0+cu121 torchvision==0.19.0+cu121 -f https://mirror.sjtu.edu.cn/pytorch-wheels/torch_stable.html
     Check "torch 安装失败，请删除 venv 文件夹后重新运行。"
-    pip install -U -I --no-deps xformers==0.0.27.post2
+    python -m pip install -U -I --no-deps xformers==0.0.27.post2
     Check "xformers 安装失败。"
 }
 
-pip install --upgrade -r requirements.txt
+python -m pip install --upgrade -r requirements.txt
 Check "sd-scripts 依赖安装失败。"
 
 Set-Location ..
-pip install --upgrade -r requirements.txt
+python -m pip install --upgrade -r requirements.txt
 Check "训练界面依赖安装失败。"
 
 Write-Output "安装完毕"
--- run_gui.ps1
+++ run_gui.ps1
@@ -1,6 +1,15 @@
-.\venv\Scripts\activate
-
 $Env:HF_HOME = "huggingface"
 $Env:PYTHONUTF8 = "1"
+
+if (Test-Path -Path "venv\Scripts\activate") {
+    Write-Host -ForegroundColor green "Activating virtual environment..."
+    .\venv\Scripts\activate
+}
+elseif (Test-Path -Path "python\python.exe") {
+    Write-Host -ForegroundColor green "Using python from python folder..."
+}
+else {
+    Write-Host -ForegroundColor Blue "No virtual environment found, using system python..."
+}
 
 python gui.py
