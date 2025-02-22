bf945edba04ba3437543e5e04d2161e152911973
Akegarasu <akiba@anzu.link>
2025-02-22 11:14:37 +0800

    update

--- run_gui_cn.sh
+++ run_gui_cn.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-
-# 激活虚拟环境
 source "./venv/bin/activate"
 
-# 环境变量
 export HF_HOME=huggingface
-# 国内hg镜像
 export HF_ENDPOINT=https://hf-mirror.com
+export PIP_INDEX_URL="https://pypi.tuna.tsinghua.edu.cn/simple"
 export PYTHONUTF8=1
 
-python gui.py --listen --tensorboard-port 6006 --port 28000 "$@"
+python gui.py "$@"
 
 
